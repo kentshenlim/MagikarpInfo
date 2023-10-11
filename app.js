97a982020abaaf2773af1541a696f624d14eb3a3
kentshenlim <kentshen_lim24@hotmail.com>
2023-10-11 18:44:37 +0800

    Move HTML into public

--- app.js
+++ app.js
@@ -9,19 +9,19 @@ const path = require('path');
   app.use(express.static('public'));
 
   app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/index.html'));
+    res.sendFile(path.join(__dirname, 'public', '/html/index.html'));
   });
 
   app.get('/about', (req, res) => {
-    res.sendFile(path.join(__dirname, '/about.html'));
+    res.sendFile(path.join(__dirname, 'public', '/html/about.html'));
   });
 
   app.get('/contact-me', (req, res) => {
-    res.sendFile(path.join(__dirname, '/contact-me.html'));
+    res.sendFile(path.join(__dirname, 'public', '/html/contact-me.html'));
   });
 
   app.use((req, res) => {
-    res.status(404).sendFile(path.join(__dirname, '404.html'));
+    res.sendFile(path.join(__dirname, 'public', '/html/404.html'));
   });
 
   app.listen(port, hostname, () => {
